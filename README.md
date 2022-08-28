301de7ae8ae23c33444b0721b916f54258574568
Hank Adler <henryaguila1988@gmail.com>
2022-08-28 13:49:38 -0400

    move editable assignments from config to .env

--- README.md
+++ README.md
@@ -27,16 +27,22 @@ NODE_OPTIONS=--experimental-vm-modules;NODE_NO_WARNINGS=1  # test config
 
 2. Set the following constants:
 
+   - ``APP`` - app/api name
+   - ``VERSION`` - app/api version, must match a directory in ``src/api``
+   - ``ENV`` - ``dev``, ``test`` or ``prod``
+   - ``HOST`` - app/api url, valid only in prod
+   - ``PORT`` - app/api server port, e.g. ``3000``
    - ``MONGO_DB_URI`` - e.g. ``mongodb://localhost:27017``
    - ``JWT_SECRET`` - jsonwebtoken secret key
+   - ``JWT_EXPIRES_IN`` - jsonwebtoken expiration time, e.g. ``1h``
+   - ``COOKIE_MAX_AGE`` - expiration in seconds for cookie storing jwt, e.g. ``3600`` for 1h
    - ``EMAIL_USER`` - address from which to send app emails
    - ``EMAIL_PASS`` - email account password
    - ``EMAIL_HOST`` - e.g. ``smtp.office365.com``
    - ``EMAIL_PORT`` - e.g. ``587``
+   - ``FAKE_PASS`` - password to give to fake users created with ``/tests/utils/setupDB.js``
 
 > outlook suggested over gmail, since gmail no longer allows direct nodemailer connection
-
-3. Revise ``config.js`` values as needed.
 
 ## Usage
 
--- src/config.js
+++ src/config.js
@@ -2,21 +2,12 @@ import { dirname } from "path";
 import { fileURLToPath } from "url";
 import "dotenv/config";
 
-// input
-const _app = "social"; // app name
-const _env = "dev"; // "dev" or "prod"
-const _apiVersion = "1"; // version string, e.g. "1", "1.2"
-const _port = "3000"; // dev port
-const _jwtExpiresIn = "1h"; // JWT validity duration
-const _cookieMaxAge = "3600"; // cookie duration in seconds
-
-// output
 const root = dirname(fileURLToPath(import.meta.url));
-const app = _app || "app";
-const env = _env || "dev";
-const api = { version: _apiVersion || "1" };
+const app = process.env.APP || "app";
+const env = process.env.ENV || "dev";
+const api = { version: process.env.VERSION || "1" };
 api.path = `/${app}/api/v${api.version}`;
-const port = _port || "3000";
+const port = process.env.PORT || "3000";
 const host = env === "prod" ? process.env.HOST : `http://localhost:${port}`;
 const db = {
   name: env === "prod" ? app : `${app}-${env}`,
@@ -26,16 +17,16 @@ const db = {
     prod: `${process.env.MONGO_DB_URI}/${app}?retryWrites=true&w=majority`
   }
 };
-const jwtExpiresIn = _jwtExpiresIn || "1h";
-const cookieMaxAge = _cookieMaxAge || "3600";
 const {
   JWT_SECRET: jwtSecret,
+  JWT_EXPIRES_IN: jwtExpiresIn,
+  COOKIE_MAX_AGE: cookieMaxAge,
   EMAIL_USER: emailUser,
   EMAIL_PASS: emailPass,
   EMAIL_HOST: emailHost,
   EMAIL_PORT: emailPort,
+  FAKE_PASS: fakePass
 } = process.env;
-const { FAKE_PASS: fakePass } = process.env;
 
 export default {
   root,
